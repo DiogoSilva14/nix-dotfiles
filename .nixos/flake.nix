c0eb6d15d7245ed3d65e7c740ef3607d686edc4a
Diogo Silva <diogo.silva.pt14@gmail.com>
2024-11-25 18:32:57 +0100

    Updated flake

--- .nixos/flake.nix
+++ .nixos/flake.nix
@@ -1,22 +1,24 @@
 {
-	description = "Nixos config flake";
+	description = "NixOS config flake";
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
 		home-manager = {
-			url = "github:nix-community/home-manager";
+			url = "github:nix-community/home-manager/release-24.11";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	};
 
-	outputs = { nixpkgs, home-manager, ... }: 
+	outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+
 		let
 			system = "x86_64-linux";
 		in {
+
 		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 			inherit system;
-			modules = [ ./configuration.nix ];
+			modules = [ ./configuration.nix	];
 		};
 
 		homeConfigurations.dps = home-manager.lib.homeManagerConfiguration {
