30e52dd079463b671bff0ce92181dc3495e6a9c2
Diogo Silva <diogo.silva.pt14@gmail.com>
2025-05-11 18:10:10 +0200

    Update gnome

--- home-manager/modules/gnome.nix
+++ home-manager/modules/gnome.nix
@@ -1,14 +1,51 @@
+{ pkgs, ... }:
 {
   dconf = {
     enable = true;
     settings = {
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
       "org/gnome/shell" = {
+        disable-user-extensions = false;
         enabled-extensions = [
-          "tiling-assistant@ubuntu.com"
-          "ubuntu-appindicators@ubuntu.com"
+          "appindicatorsupport@rgcjonas.gmail.com"
+          "tiling-assistant@leleat-on-github"
         ];
+        favorite-apps = [
+          "brave-browser.desktop"
+          "org.gnome.Nautilus.desktop"
+          "spotify.desktop"
+          "kitty.desktop"
+        ];
+      };
+
+      "org/gnome/desktop/background" = {
+        picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/map-d.svg";
+      };
+
+      "org/gnome/desktop/screensaver" = {
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/map-d.svg";
+      };
+
+      "org/gnome/desktop/interface" = {
+        show-battery-percentage = true;
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        ];
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Super>t";
+        command = "kitty";
+        name = "Shell";
       };
     };
   };
+
+  home.packages = with pkgs; [
+    gnomeExtensions.tiling-assistant
+    gnomeExtensions.appindicator
+  ];
 }
